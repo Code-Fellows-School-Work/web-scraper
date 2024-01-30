959c2c452584f7fcb8e061993c7ffb153a7b1be8
evidad <errolvidad@gmail.com>
2024-01-30 14:44:50 -0800

    added citation report function

--- web_scapper/requests_scrapper.py
+++ web_scapper/requests_scrapper.py
@@ -9,7 +9,13 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 def get_citations_needed_count():
     """
-    Counts the amount of "citation needed" tags on a wikipedia page and returns an integer
+    Counts the amount of "citation needed" tags on a wikipedia page
+
+    Param: 
+        url string
+
+    Output:
+        Integer
     """
     
     citations = soup.find_all(string="citation needed")
@@ -19,6 +25,20 @@ def get_citations_needed_count():
     
         citation_count.append(citation)
     
-    print(f"The amount of citations '{url}' webpage needs is", len(citation_count))
+    print(f"The amount of citations '{url}' needs is {len(citation_count)}.")
 
 get_citations_needed_count()
+
+def get_citations_needed_report():
+
+    # Used ChatGPT to figure out the CSS selector
+    citation_reference = soup.select('a[href="/wiki/Wikipedia:Citation_needed"][title="Wikipedia:Citation needed"]')
+
+    for citation_text in citation_reference:
+        parent_tag = citation_text.find_parent('p')
+        if parent_tag:
+            print(parent_tag.text)
+        else:
+            print("No citation needed tags found")
+
+get_citations_needed_report()
